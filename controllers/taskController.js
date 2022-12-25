00b396c4457bbf1d2c78f6bf7dcb71472ed6e8cc
Redditask <artyomageyko@gmail.com>
2022-12-25 17:20:34 +0300

    refactoring task model and controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,40 +2,65 @@ const {Task} = require("../models/models");
 const ApiError = require("../error/ApiError");
 
 class TaskController {
-    async create(req, res){
-        const {id, ...rest} = req.body;
-        if (id && rest){
-            await Task.create({id: id, taskData: rest});
+    async create(req, res, next) {
+        const {id, taskText, year, month, day, startTime, endTime, color} = req.body;
+        try {
+            await Task.create({
+                id: id,
+                taskText: taskText,
+                year: year,
+                month: month,
+                day: day,
+                startHour: startTime.hour,
+                startMin: startTime.min,
+                endHour: endTime.hour,
+                endMin: endTime.min,
+                color: color,
+            });
             return res.status(200).json({message: "Задача успешно добавлена"});
+        } catch (e) {
+            return next(ApiError.badRequest("Запрос передан некорректно"));
         }
-
-        return next(ApiError.badRequest("Запрос передан некорректно"));
     };
 
-    async getAll(req, res){
+    async getAll(req, res) {
         const tasks = await Task.findAll();
         return res.status(200).json(tasks);
     };
 
-    async delete(req, res, next){
-        const deleted = await Task.destroy({where: {id: req.params.id}});
-        if(deleted) {
+    async delete(req, res, next) {
+        try {
+            await Task.destroy({where: {id: req.params.id}});
             return res.status(200).json({message: "Задача успешно удалена"});
+        } catch (e) {
+            return next(ApiError.notFound("Задача с таким id не найдена"));
         }
-
-        return next(ApiError.notFound("Задача с таким id не найдена"));
     };
 
-    async update(req, res, next){
-        if(req.body) {
-            const update = await Task.update({taskData: req.body}, {where: {id: req.params.id}});
+    async update(req, res, next) {
+        try {
+            const {taskText, year, month, day, startTime, endTime, color} = req.body;
+            const update = await Task.update(
+                {
+                    taskText: taskText,
+                    year: year,
+                    month: month,
+                    day: day,
+                    startHour: startTime.hour,
+                    startMin: startTime.min,
+                    endHour: endTime.hour,
+                    endMin: endTime.min,
+                    color: color,
+                },
+                {where: {id: req.params.id}},
+            );
             if (update) {
                 return res.status(200).json({message: "Задача успешно обновлена"});
             }
-
             return next(ApiError.notFound("Задача с таким id не найдена"));
+        } catch (e) {
+            return next(ApiError.badRequest("Запрос передан некорректно"));
         }
-        return next(ApiError.badRequest("Запрос передан некорректно"));
     };
 }
 
--- models/models.js
+++ models/models.js
@@ -9,7 +9,15 @@ const User = sequelize.define("user", {
 
 const Task = sequelize.define("task", {
     id: {type: DataTypes.STRING, primaryKey: true, unique: true},
-    taskData: {type: DataTypes.JSON},
+    taskText: {type: DataTypes.STRING},
+    year: {type: DataTypes.INTEGER},
+    month: {type: DataTypes.INTEGER},
+    day: {type: DataTypes.INTEGER},
+    startHour: {type: DataTypes.INTEGER},
+    startMin: {type: DataTypes.INTEGER},
+    endHour: {type: DataTypes.INTEGER},
+    endMin: {type: DataTypes.INTEGER},
+    color: {type: DataTypes.STRING},
 });
 
 User.hasMany(Task);
