73ead9ac8d2f86e87c6b4505f33357d321bdb1c9
Redditask <artyomageyko@gmail.com>
2022-12-25 16:34:46 +0300

    finished task crud methods

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,43 +3,39 @@ const ApiError = require("../error/ApiError");
 
 class TaskController {
     async create(req, res){
-        // добавить проверку на передаваемые данные
         const {id, ...rest} = req.body;
-        const task = await Task.create({id: id, taskData: rest});
-        return res.json(task);
+        if (id && rest){
+            await Task.create({id: id, taskData: rest});
+            return res.status(200).json({message: "Задача успешно добавлена"});
+        }
+
+        return next(ApiError.badRequest("Запрос передан некорректно"));
     };
 
     async getAll(req, res){
         const tasks = await Task.findAll();
-        return res.json(tasks);
+        return res.status(200).json(tasks);
     };
 
     async delete(req, res, next){
-        const {id} = req.params;
-        const tasks = await Task.findAll();
-        const deletedTask = tasks.find(task => task.dataValues.id === id);
-
-        if(deletedTask) {
+        const deleted = await Task.destroy({where: {id: req.params.id}});
+        if(deleted) {
             return res.status(200).json({message: "Задача успешно удалена"});
         }
-        return next(ApiError.badRequest("Задача с таким id не найдена"));
+
+        return next(ApiError.notFound("Задача с таким id не найдена"));
     };
 
     async update(req, res, next){
-        const {id} = req.params;
-        const tasks = await Task.findAll();
-        tasks.forEach(task => {
-            if (task.dataValues.id === id){
-                task.dataValues.taskData = req.body;
+        if(req.body) {
+            const update = await Task.update({taskData: req.body}, {where: {id: req.params.id}});
+            if (update) {
                 return res.status(200).json({message: "Задача успешно обновлена"});
             }
-        });
 
-        return next(ApiError.badRequest("Задача с таким id не найдена"));
-    };
-
-    async getOne(req, res){
-        //возможно и не нужно
+            return next(ApiError.notFound("Задача с таким id не найдена"));
+        }
+        return next(ApiError.badRequest("Запрос передан некорректно"));
     };
 }
 
