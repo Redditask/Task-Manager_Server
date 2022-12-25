08bfd6db0811cc78835caeb441f7e5f41d13179d
Redditask <artyomageyko@gmail.com>
2022-12-25 17:58:49 +0300

    created user registration and authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,59 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {User} = require("../models/models");
 
 class UserController {
-    async registration(req, res){
+    async registration(req, res, next){
+        const {login, password} = req.body;
+        //ToDo: сделать нормальную валидацию
+        if(login && password){
+            const isUser = await User.findOne({where: {login}});
+            if(isUser){
+                return next(ApiError.badRequest("Пользователь уже существует"));
+            }
 
+            const hashPassword = await bcrypt.hash(password, 4);
+            const user = await User.create({login, password: hashPassword});
+            const token = jwt.sign(
+                {id: user.id, login},
+                process.env.SECRET_KEY,
+                {expiresIn: "24h"},
+            );
+
+            return res.json({token});
+        }
+
+        return next(ApiError.badRequest("Данные для регистрации некорректны"));
     };
 
-    async login(req, res){
+    async login(req, res, next){
+        const {login, password} = req.body;
+        if (login && password){
+            const isUser = await User.findOne({where: {login}});
+            if (isUser){
+                const isValidPassword = bcrypt.compareSync(password, isUser.password);
+                if (isValidPassword){
+                    const token = jwt.sign(
+                        {id: isUser.id, login: isUser.login},
+                        process.env.SECRET_KEY,
+                        {expiresIn: "24h"},
+                    );
 
+                    return res.json({token});
+                }
+
+                return next(ApiError.badRequest("Указан неверный пароль"));
+            }
+
+            return next(ApiError.notFound("Такого пользователя не существует"));
+        }
+
+        return next(ApiError.badRequest("Данные для входа некорректны"));
     };
 
     async check(req, res, next){
-        const {id} = req.query;
-        if(!id){
-            return next(ApiError.badRequest("id пользователя не задан"));
-        }
-        res.json(id);
+
     };
 }
 
--- models/models.js
+++ models/models.js
@@ -3,7 +3,7 @@ const {DataTypes} = require("sequelize");
 
 const User = sequelize.define("user", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING, unique: true},
+    login: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
 });
 
